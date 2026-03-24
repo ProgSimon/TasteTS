8099ec7afd8886d0887f15bb5d2616aca1ba22ee
Szymon Szota <szymonszota@gmail.com>
2026-03-24 21:50:19 +0100

    api: added implemented routers to tRPC root

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,9 @@
 import { recipeRouter } from "~/server/api/routers/recipe";
+import { ingredientRouter } from "./routers/ingredient";
+import { pendingIngredientsRouter } from "./routers/pendingIngredient";
+import { reviewRouter } from "./routers/reviews";
+import { tagRouter } from "./routers/tag";
+import { alternativeRouter } from "./routers/alternative";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +12,12 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  recipe: recipeRouter
+  recipe: recipeRouter,
+  ingredient: ingredientRouter,
+  pendingIngredient: pendingIngredientsRouter,
+  review: reviewRouter,
+  tag: tagRouter,
+  alternative: alternativeRouter
 });
 
 // export type definition of API
